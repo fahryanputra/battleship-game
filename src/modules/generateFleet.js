e47eab036ec619260d39cffb75c65fccadc0cb2b
fahryanputra <fahryanputra@gmail.com>
2024-03-15 06:39:17 +0700

    Add feature to generate ship position

---
+++ src/modules/generateFleet.js
@@ -0,0 +1,32 @@
+import generateRandomNumber from "../utilities/generateRandomNumber";
+
+function addShip(player, name, length = 1) {
+  player.getGameboard().addShip(name, length);
+}
+
+function placeShip(player, name, x, y, axis = 0) {
+  player.getGameboard().placeShip(name, x, y, axis);
+}
+
+function generateFleet(player) {
+  addShip(player, "destroyer", 2);
+
+  const axis = generateRandomNumber(2);
+  let x = 0;
+  let y = 0;
+
+  if (axis < 1) {
+    x = generateRandomNumber(3);
+    y = generateRandomNumber(2);
+  } else {
+    x = generateRandomNumber(2);
+    y = generateRandomNumber(3);
+  }
+
+  console.log(axis);
+  console.log(`${x}, ${y}`);
+
+  placeShip(player, "destroyer", x, y, axis);
+}
+
+export default generateFleet;
