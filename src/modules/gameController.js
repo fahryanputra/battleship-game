3bd369614f49cf9732e2bc4a2e064968cffbf9c0
fahryanputra <fahryanputra@gmail.com>
2024-03-14 07:30:56 +0700

    Add player turn and attack system

--- src/modules/gameController.js
+++ src/modules/gameController.js
@@ -1,14 +1,85 @@
 import Player from "./Player";
 
-function gameController() {
-  const player = new Player("player", true);
-  const computer = new Player("computer");
-  computer.getGameboard().addShip("destroyer");
-  computer.getGameboard().placeShip("destroyer", 0, 0, 0);
+function pickRandomNumber(size) {
+  return Math.floor(Math.random() * size);
+}
 
+function createPlayer(array, name, isPlayer = false) {
+  const player = new Player(name, isPlayer);
+  array.push(player);
+}
+
+function addShip(player, name, length = 1) {
+  player.getGameboard().addShip(name, length);
+}
+
+function placeShip(player, name, x, y, axis = 0) {
+  player.getGameboard().placeShip(name, x, y, axis);
+}
+
+function attackValidation(player, x, y) {
+  switch (player.getGameboard().getBoard()[x][y]) {
+    case 1:
+    case 2:
+      return false;
+
+    default:
+      return true;
+  }
+}
+
+function playerTurn(player, computer) {
+  alert(`${player.getName()}'s turn!`);
+  let x = 0;
+  let y = 0;
+  let isValid = false;
+
+  while (!isValid) {
+    x = prompt("enter x coordinate:");
+    y = prompt("enter y coordinate:");
+
+    isValid = attackValidation(computer, x, y);
+
+    if (!isValid) alert("invalid location, try again!");
+  }
+
+  player.attack(computer, x, y);
+}
+
+function computerTurn(computer, player) {
+  alert(`${computer.getName()}'s turn!`);
+  let x = 0;
+  let y = 0;
+
+  let isValid = false;
+  while (!isValid) {
+    x = pickRandomNumber(3);
+    y = pickRandomNumber(3);
+
+    isValid = attackValidation(player, x, y);
+  }
+
+  computer.attack(player, x, y);
+
+  alert(`${computer.getName()} attack ${x},${y}`);
+}
+
+function gameController() {
   const players = [];
-  players.push(player);
-  players.push(computer);
+  createPlayer(players, "player", true);
+  createPlayer(players, "computer");
+
+  addShip(players[1], "destroyer", 2);
+  placeShip(players[1], "destroyer", 0, 0, 0);
+
+  let round = 0;
+
+  while (round < 3) {
+    playerTurn(players[0], players[1]);
+    computerTurn(players[1], players[0]);
+
+    round += 1;
+  }
 }
 
 export default gameController;
