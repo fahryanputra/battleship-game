167e26529c63fe1237bc769ceaf9b63788125ba4
fahryanputra <fahryanputra@gmail.com>
2024-03-16 07:07:34 +0700

    Fix gameboard test

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -3,11 +3,11 @@ import Gameboard from "../modules/Gameboard";
 import Ship from "../modules/Ship";
 
 const gameboard = new Gameboard(3, 3);
-const destroyer = new Ship("destroyer", 2);
-const cruiser = new Ship("cruiser", 3);
 
 gameboard.addShip("destroyer", 2);
 gameboard.addShip("cruiser", 3);
+
+const fleet = gameboard.getShips();
 
 describe("Gameboard class", () => {
   test("create board", () => {
@@ -18,54 +18,46 @@ describe("Gameboard class", () => {
     ]);
   });
 
+  test("check ship object", () => {
+    expect(gameboard.getShips()[0]).toMatchSnapshot({
+      name: "destroyer",
+    });
+  });
+
   test("place ship", () => {
-    gameboard.placeShip("destroyer", 0, 0, 0);
-    expect(gameboard.getBoard()).toStrictEqual([
-      [destroyer, destroyer, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ]);
+    gameboard.placeShip(fleet[0], 0, 0, 0);
+    for (let index = 0; index < fleet[0].length; index++) {
+      expect(gameboard.getBoard()[0][index]).toMatchSnapshot({
+        name: "destroyer",
+      });
+    }
   });
 
   test("place another ship", () => {
-    gameboard.placeShip("cruiser", 0, 2, 1);
-    expect(gameboard.getBoard()).toStrictEqual([
-      [destroyer, destroyer, cruiser],
-      [0, 0, cruiser],
-      [0, 0, cruiser],
-    ]);
+    gameboard.placeShip(fleet[1], 0, 2, 1);
+    for (let index = 0; index < fleet[1].length; index++) {
+      expect(gameboard.getBoard()[index][2]).toMatchSnapshot({
+        name: "cruiser",
+      });
+    }
   });
 
   test("record missed shot", () => {
     gameboard.receiveAttack(1, 1);
-    expect(gameboard.getBoard()).toStrictEqual([
-      [destroyer, destroyer, cruiser],
-      [0, 1, cruiser],
-      [0, 0, cruiser],
-    ]);
+    expect(gameboard.getBoard()[1][1]).toBe(1);
   });
 
   test("record ship hit", () => {
     gameboard.receiveAttack(0, 0);
-    destroyer.hit();
-    expect(gameboard.getShips()[0].getDamage()).toBe(1);
-    expect(gameboard.getBoard()).toStrictEqual([
-      [2, destroyer, cruiser],
-      [0, 1, cruiser],
-      [0, 0, cruiser],
-    ]);
+    expect(fleet[0].getDamage()).toBe(1);
+    expect(gameboard.getBoard()[0][0]).toBe(2);
   });
 
   test("sunk ship", () => {
     gameboard.receiveAttack(0, 1);
-    destroyer.hit();
-    expect(gameboard.getBoard()).toStrictEqual([
-      [2, 2, cruiser],
-      [0, 1, cruiser],
-      [0, 0, cruiser],
-    ]);
-    expect(gameboard.getShips()[0].getDamage()).toBe(2);
-    expect(gameboard.getShips()[0].getIsSunk()).toBe(true);
+    expect(gameboard.getBoard()[0][1]).toBe(2);
+    expect(fleet[0].getDamage()).toBe(2);
+    expect(fleet[0].getIsSunk()).toBe(true);
     expect(gameboard.getSunkMessage()).toBe("destroyer sunk");
     expect(gameboard.getAvailableShips().length).toBe(1);
   });
@@ -77,7 +69,6 @@ describe("Gameboard class", () => {
 
   test("attack hit message", () => {
     gameboard.receiveAttack(0, 2);
-    cruiser.hit();
     expect(gameboard.getAttackMessage()).toBe("hit");
   });
 
@@ -89,8 +80,6 @@ describe("Gameboard class", () => {
   test("all ship sunk", () => {
     gameboard.receiveAttack(1, 2);
     gameboard.receiveAttack(2, 2);
-    cruiser.hit();
-    cruiser.hit();
     expect(gameboard.getIsAllShipSunk()).toBe(true);
   });
 });
